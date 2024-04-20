723334bd63aaecc55bf6166e8b9ecf2255ef398d
Betty <qwe50929@gmail.com>
2024-04-21 01:03:39 +0800

    feat: Set userStore

--- repli_frontend/src/App.vue
+++ repli_frontend/src/App.vue
@@ -1,9 +1,29 @@
 <script>
 import Toast from '@/components/Toast.vue';
+import { useUserStore } from '@/stores/user.js';
+import axios from 'axios';
 
 export default {
+	setup() {
+		const userStore = useUserStore();
+
+		return {
+			userStore,
+		};
+	},
 	components: {
 		Toast,
+	},
+	beforeCreate() {
+		this.userStore.initStore();
+
+		const token = this.userStore.user.access;
+
+		if (token) {
+			axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+		} else {
+			axios.defaults.headers.common['Authorization'] = '';
+		}
 	},
 };
 </script>
