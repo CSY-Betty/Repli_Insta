be5ccead4ed0517a4e1918428d72417540ea7d84
Betty <qwe50929@gmail.com>
2024-04-21 01:01:47 +0800

    chore: Update REST framework configuration for permission classes

--- repli_backend/repli_backend/settings.py
+++ repli_backend/repli_backend/settings.py
@@ -45,9 +45,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
 
 CORS_ALLOWED_ORIGINS = [
