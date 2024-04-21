e60c7d3939905ac6c0bcf00ab1b915c645c0e1f1
Betty <qwe50929@gmail.com>
2024-04-21 17:01:55 +0800

    feat: Add post show

--- repli_frontend/src/views/ExploreView.vue
+++ repli_frontend/src/views/ExploreView.vue
@@ -1,4 +1,6 @@
 <script>
+import axios from 'axios';
+
 import PostsList from '@/components/PostsList.vue';
 
 export default {
@@ -6,10 +8,36 @@ export default {
 	components: {
 		PostsList,
 	},
+	data() {
+		return {
+			posts: [],
+		};
+	},
+	mounted() {
+		this.getPost();
+	},
+	methods: {
+		getPost() {
+			axios
+				.get('/api/posts')
+				.then((response) => {
+					console.log('data', response.data);
+					this.posts = response.data;
+				})
+				.catch((error) => {
+					console.log('error', error);
+				});
+		},
+	},
 };
 </script>
 <template>
 	<div class="flex-col justify-center w-full mt-6 px-40 overflow-y-auto">
+		<div v-for="post in posts" v-bind:key="post.id">
+			{{ post.body }}
+			{{ post.created_by.name }}
+			{{ post.created_at_formatted }} ago
+		</div>
 		<ul class="grid grid-cols-3 gap-1">
 			<PostsList />
 		</ul>
