0431fd6af9f3a8fb0643216ccfe01b3d6b2453d0
Betty <qwe50929@gmail.com>
2024-04-21 21:04:21 +0800

    feat: Add post create url

--- repli_backend/post/urls.py
+++ repli_backend/post/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import api
 
-urlpatterns = [path("", api.post_list, name="post_list")]
+urlpatterns = [
+    path("", api.post_list, name="post_list"),
+    path("create/", api.post_create, name="post_create"),
+]
