c20995b1783ac6bf98e3e322de97710f1cc3890d
Betty <qwe50929@gmail.com>
2024-04-24 14:38:35 +0800

    fix: friend request multiple

--- repli_backend/account/api.py
+++ repli_backend/account/api.py
@@ -54,7 +54,9 @@ def friends(request, pk):
 
     # 如果發送請求者是帳戶使用者，取得帳戶使用者收到的好友請求
     if user == request.user:
-        requests = FriendshipRequest.objects.filter(created_for=request.user)
+        requests = FriendshipRequest.objects.filter(
+            created_for=request.user, status=FriendshipRequest.SENT
+        )
 
     # 獲取特定用戶的好友資料
     friends = user.friends.all()
@@ -73,6 +75,45 @@ def friends(request, pk):
 def send_friendship_request(request, pk):
     user = User.objects.get(pk=pk)
 
-    friendship_request = FriendshipRequest(created_for=user, created_by=request.user)
+    # 讀取接收者的清單與邀請者的清單是否已經存在相同的邀請
+    check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(
+        created_by=user
+    )
+    check2 = FriendshipRequest.objects.filter(created_for=user).filter(
+        created_by=request.user
+    )
 
-    return JsonResponse({"friendship request": "friendship request created"})
+    if not check1 or not check2:
+        friendship_request = FriendshipRequest.objects.create(
+            created_for=user, created_by=request.user
+        )
+
+        return JsonResponse({"friendship request": "friendship request created."})
+
+    else:
+        return JsonResponse({"friendship request": "request already sent."})
+
+
+@api_view(["POST"])
+def handle_request(request, pk, status):
+    # 取得申請者的資料
+    user = User.objects.get(pk=pk)
+
+    # 讀取好友資料
+    friendship_request = FriendshipRequest.objects.filter(created_for=request.user).get(
+        created_by=user
+    )
+
+    # 修改好友狀態
+    friendship_request.status = status
+    friendship_request.save()
+
+    user.friends.add(request.user)
+    user.friends_count = user.friends_count + 1
+    user.save()
+
+    request_user = request.user
+    request_user.friends_count = request_user.friends_count + 1
+    request_user.save()
+
+    return JsonResponse({"message": "friendship request updated"})
