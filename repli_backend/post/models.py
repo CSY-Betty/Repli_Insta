331100c2f142205fb2f96f0e11d06f6f5ba67f9d
Betty <qwe50929@gmail.com>
2024-04-24 14:46:11 +0800

    feat: Add post likes count

--- repli_backend/post/models.py
+++ repli_backend/post/models.py
@@ -4,6 +4,12 @@ from account.models import User
 from django.utils.timesince import timesince
 
 
+class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 # Create your models here.
 class PostAttachment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -18,6 +24,9 @@ class Post(models.Model):
     body = models.TextField(blank=True, null=True)
 
     attachments = models.ManyToManyField(PostAttachment, blank=True)
+
+    likes = models.ManyToManyField(Like, blank=True)
+    likes_count = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
