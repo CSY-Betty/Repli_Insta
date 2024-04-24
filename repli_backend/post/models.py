bc96baebb598760b613a108740ec746fe6329a64
Betty <qwe50929@gmail.com>
2024-04-25 00:50:46 +0800

    feat: Add comment Model

--- repli_backend/post/models.py
+++ repli_backend/post/models.py
@@ -10,6 +10,15 @@ class Like(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, related_name="comments", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 # Create your models here.
 class PostAttachment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -27,6 +36,9 @@ class Post(models.Model):
 
     likes = models.ManyToManyField(Like, blank=True)
     likes_count = models.IntegerField(default=0)
+
+    comments = models.ManyToManyField(Comment, blank=True)
+    comments_count = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
