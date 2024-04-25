99121aaa7187a4e84a9848b85ca6ed5381c4560d
Betty <qwe50929@gmail.com>
2024-04-25 11:21:34 +0800

    feat: Add chat app

--- repli_backend/chat/models.py
+++ repli_backend/chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from account.models import User
+
 # Create your models here.
+
+
+class Conversation(models.Model):
+    user = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ConcersationMessage(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, related_name="messages", on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, related_name="sent_messages", on_delete=models.CASCADE
+    )
+    sent_to = models.ForeignKey(
+        User, related_name="received_messages", on_delete=models.CASCADE
+    )
