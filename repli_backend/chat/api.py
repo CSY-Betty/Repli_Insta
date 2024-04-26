15a23fba078b64960ce30988724d9b4cf3815e8b
Betty <qwe50929@gmail.com>
2024-04-26 10:44:09 +0800

    feat: Create conversation view

--- repli_backend/chat/api.py
+++ repli_backend/chat/api.py
@@ -19,6 +19,17 @@ from .serializers import (
 def conversation_list(request):
     conversations = Conversation.objects.filter(users__in=list([request.user]))
 
-    print(conversations)
+    serializer = ConversationSerializer(conversations, many=True)
 
-    return JsonResponse({"message": "conversation load success"})
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(["GET"])
+def conversation_detail(request, pk):
+    conversation = Conversation.objects.filter(users__in=list([request.user])).get(
+        pk=pk
+    )
+
+    serializer = ConversationDetailSeriazlizer(conversation)
+
+    return JsonResponse(serializer.data, safe=False)
--- repli_frontend/src/views/MessagesView.vue
+++ repli_frontend/src/views/MessagesView.vue
@@ -1,19 +1,78 @@
 <script>
-import FriendsList from '@/components/FriendsList.vue';
+import axios from 'axios';
+import { useUserStore } from '@/stores/user';
 
 export default {
 	name: 'MessagesView',
-	components: {
-		FriendsList,
+	setup() {
+		const userStore = useUserStore();
+		return {
+			userStore,
+		};
+	},
+	data() {
+		return {
+			conversations: [],
+			activeConversation: {},
+		};
+	},
+
+	mounted() {
+		this.getConversations();
+	},
+	methods: {
+		getConversations() {
+			console.log('getConversations');
+			axios
+				.get('/api/chat/')
+				.then((response) => {
+					console.log(response.data);
+					this.conversations = response.data;
+
+					if (this.conversations) {
+						this.activeConversation = this.conversations[0];
+					}
+
+					this.getMessages();
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
+		getMessages() {
+			console.log('getMessages');
+			axios
+				.get(`/api/chat/${this.activeConversation.id}/`)
+				.then((response) => {
+					console.log(response.data);
+					this.activeConversation = response.data;
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
 	},
 };
 </script>
 <template>
 	<main class="grid grid-cols-4 w-full">
 		<article
+			v-for="conversation in conversations"
+			v-bind:key="conversation.id"
 			class="col-span-1 justify-center pt-6 overflow-y-auto border-r h-screen"
 		>
-			<FriendsList />
+			<template v-for="user in conversation.users" v-bind:key="user.id">
+				<button
+					v-if="user.id !== userStore.user.id"
+					class="px-10 py-2 w-full flex items-center cursor-pointer hover:bg-gray-50 bg-white text-black focus:bg-gray-100"
+				>
+					<img
+						src="https://i.pravatar.cc/150?img=29"
+						class="w-16 aspect-square rounded-full"
+					/>
+					<p class="ml-2 text-xl">{{ user.name }}</p>
+				</button>
+			</template>
 		</article>
 		<section class="col-span-3 pt-6 h-screen">
 			<div class="grid grid-rows-10 h-full">
@@ -31,7 +90,11 @@ export default {
 				</div>
 				<!-- Chat bubble -->
 				<div class="mt-2 row-span-8 overflow-y-auto">
-					<div class="grid gap-2">
+					<div
+						v-for="message in activeConversation.messages"
+						v-bind:key="message.id"
+						class="grid gap-2"
+					>
 						<!-- Chat bubble sender -->
 						<div class="flex items-end gap-2.5 pl-4">
 							<img
@@ -45,8 +108,7 @@ export default {
 								<p
 									class="text-sm font-normal py-2.5 text-gray-900 dark:text-white"
 								>
-									That's awesome. I think our users will
-									really appreciate the improvements.
+									{{ message.body }}
 								</p>
 							</div>
 						</div>
