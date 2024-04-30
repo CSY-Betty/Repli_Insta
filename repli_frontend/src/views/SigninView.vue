a48215a3d20de66c1313644e6d0c9f040374bc93
Betty <qwe50929@gmail.com>
2024-04-30 17:16:04 +0800

    feat: Add default text

--- repli_frontend/src/views/SigninView.vue
+++ repli_frontend/src/views/SigninView.vue
@@ -13,8 +13,8 @@ export default {
 	data() {
 		return {
 			form: {
-				email: '',
-				password: '',
+				email: 'repli@gmail.com',
+				password: 'repliinsta',
 			},
 			errors: [],
 		};
@@ -113,26 +113,8 @@ export default {
 								value="repliinsta"
 								required
 							/>
-							<a
-								href="#"
-								class="text-xs text-gray-600 hover:text-rose-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-								>Forgot Password?</a
-							>
 						</div>
 						<div class="flex items-center justify-between mb-4">
-							<div class="flex items-center">
-								<input
-									type="checkbox"
-									id="remember"
-									class="h-4 w-4 rounded border-gray-300 text-rose-600 focus:ring-rose-500 focus:outline-none"
-									checked
-								/>
-								<label
-									for="remember"
-									class="ml-2 block text-sm text-gray-700 dark:text-gray-300"
-									>Remember me</label
-								>
-							</div>
 							<RouterLink
 								to="/signup"
 								class="text-xs text-black hover:text-rose-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
