d9d5046b3ea399233daa5b83d45e395ad225922d
Betty <qwe50929@gmail.com>
2024-05-14 17:52:53 +0800

    feat: Set chat models

--- repli_backend/chat/models.py
+++ repli_backend/chat/models.py
@@ -7,40 +7,16 @@ from account.models import User
 # Create your models here.
 
 
-class Conversation(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    users = models.ManyToManyField(User, related_name="conversations")
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-
-    def modified_at_formatted(self):
-        return timesince(self.modified_at)
-
-
-class ConversationMessage(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    conversation = models.ForeignKey(
-        Conversation, related_name="messages", on_delete=models.CASCADE
-    )
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(
-        User, related_name="sent_messages", on_delete=models.CASCADE
-    )
-    sent_to = models.ForeignKey(
-        User, related_name="received_messages", on_delete=models.CASCADE
-    )
-
-    def created_at_formatted(self):
-        return timesince(self.created_at)
-
-
 class Message(models.Model):
-    body = models.TimeField()
+    body = models.TextField()
     sent_by = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        User, blank=True, null=True, on_delete=models.SET_NULL
+        User,
+        related_name="created_by",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
     )
 
     class Meta:
@@ -51,24 +27,20 @@ class Message(models.Model):
 
 
 class Room(models.Model):
-    WAITING = "waiting"
-    ACTIVE = "active"
-    CLOSED = "closed"
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants1 = models.ManyToManyField(User, related_name="participants1")
+    participants2 = models.ManyToManyField(User, related_name="participants2")
 
-    CHOICE_STATUS = ((WAITING, "Waiting"), (ACTIVE, "Active"), (CLOSED, "Closed"))
-
-    uuid = models.CharField(max_length=255)
-    client = models.CharField(max_length=255)
-    agent = models.ForeignKey(
-        User, related_name="rooms", blank=True, null=True, on_delete=models.SET_NULL
-    )
     messages = models.ManyToManyField(Message, blank=True)
-    url = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(max_length=20, choices=CHOICE_STATUS, default=WAITING)
+
     created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def modified_at_formatted(self):
+        return timesince(self.modified_at)
 
     class Meta:
         ordering = ("-created_at",)
 
     def __str__(self) -> str:
-        return f"{self.client} - {self.uuid}"
+        return f"{self.participants1} - {self.id}"
